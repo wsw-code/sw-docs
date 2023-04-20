3346bfb2e82344868cf31fbc2c60953067652900
wsw-code <602851406@qq.com>
2023-04-20 23:43:27 +0800

    feat: 完善preWrapper.ts

--- src/node/plugin-mdx/rehypePlugins/preWrapper.ts
+++ src/node/plugin-mdx/rehypePlugins/preWrapper.ts
@@ -7,8 +7,51 @@ export const rehypePluginPreWrapper: Plugin<[], Root> = () => {
     visit(tree, 'element', (node) => {
       // <pre><code>...</code></pre>
       // 1. 找到 pre 元素
-      // 2. 解析出代码的语言名称
-      // 3. 变换 Html AST
+      if (
+        node.tagName === 'pre' &&
+        node.children[0]?.type === 'element' &&
+        node.children[0].tagName === 'code' &&
+        !node.data?.isVisited
+      ) {
+        console.log(node.tagName);
+        const codeNode = node.children[0];
+        const codeClassName = codeNode.properties?.className?.toString() || '';
+        // language-xxx
+        const lang = codeClassName.split('-')[1];
+
+        codeNode.properties.className = '';
+
+        const clonedNode: Element = {
+          type: 'element',
+          tagName: 'pre',
+          children: node.children,
+          data: {
+            isVisited: true
+          }
+        };
+
+        // 修改原来的 pre 标签 -> div 标签
+        node.tagName = 'div';
+        node.properties = node.properties || {};
+        node.properties.className = codeClassName;
+
+        node.children = [
+          {
+            type: 'element',
+            tagName: 'span',
+            properties: {
+              className: 'lang'
+            },
+            children: [
+              {
+                type: 'text',
+                value: lang
+              }
+            ]
+          },
+          clonedNode
+        ];
+      }
     });
   };
 };
